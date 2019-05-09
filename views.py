3d87eb1b5761828d4077a376011ca55e2f99e41f
anesas8811 <50436737+anesas8811@users.noreply.github.com>
2019-05-09 21:05:04 +0530

    Update views.py

--- views.py
+++ views.py
@@ -5,4 +5,4 @@ source = 'https://github.com/zeit/now-examples/tree/master/python-django'
 css = '<link rel="stylesheet" href="/css/style.css" />'
 
 def index(request):
-    return HttpResponse("%s<h1>Welcome to gaming.xyz</h1><p>Want To Buy This Domain ?  Contact me ! Price Negotiable !</p><p>Contact me <a href='https://www.facebook.com/mrvincere1'>Here ! </a> </p>" % (css, source), content_type='text/html')
+    return HttpResponse("%s<h1>Welcome to gaming.xyz</h1><p>Want To Buy This Domain ?  Contact me ! Price Negotiable !</p><p>Contact me <a href='https://www.facebook.com/mrvincere1'>Here ! </a> </p>" % (css), content_type='text/html')
